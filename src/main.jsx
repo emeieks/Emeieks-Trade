27e4254715ad69eeda6987d611cb26f06ccf0f7a
emeieks <maxime.bergeron91@gmail.com>
2026-08-01 16:15:48 +0700

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./TradingJournalPro.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-)
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
